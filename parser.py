c0bb20336209bb1faf4e5df28248d7edd0099085
BaldLee <baldlee@qq.com>
2020-10-11 18:24:38 +0800

    no lex error

--- parser.py
+++ parser.py
@@ -8,26 +8,26 @@ from defination import *
 #
 #
 
-reserved = {
-    'after': 'AFTER',
-    'apply': 'APPLAY',
-    'attributes': 'ATTRIBUTES',
-    'call': 'CALL',
-    'case': 'CASE',
-    'catch': 'CATCH',
-    'do': 'DO',
-    'end': 'END',
-    'fun': 'FUN',
-    'in': 'IN',
-    'let': 'LET',
-    'letrec': 'LETREC',
-    'module': 'MODEULE',
-    'of': 'OF',
-    'primop': 'PRIMOP',
-    'receive': 'RECEIVE',
-    'try': 'TRY',
-    'when': 'WHEN'
-}
+# reserved = {
+#     'after': 'AFTER',
+#     'apply': 'APPLAY',
+#     'attributes': 'ATTRIBUTES',
+#     'call': 'CALL',
+#     'case': 'CASE',
+#     'catch': 'CATCH',
+#     'do': 'DO',
+#     'end': 'END',
+#     'fun': 'FUN',
+#     'in': 'IN',
+#     'let': 'LET',
+#     'letrec': 'LETREC',
+#     'module': 'MODULE',
+#     'of': 'OF',
+#     'primop': 'PRIMOP',
+#     'receive': 'RECEIVE',
+#     'try': 'TRY',
+#     'when': 'WHEN'
+# }
 
 tokens = [
     'ATOM',
@@ -56,8 +56,26 @@ tokens = [
     'MINUS',
     'MULTI',
     'EXCLAIM',
-    'APO'
-] + list(reserved.values())
+    'APO',
+    'AFTER',
+    'APPLY',
+    'ATTRIBUTES',
+    'CALL',
+    'CASE',
+    'CATCH',
+    'DO',
+    'END',
+    'FUN',
+    'IN',
+    'LET',
+    'LETREC',
+    'MODULE',
+    'OF',
+    'PRIMOP',
+    'RECEIVE',
+    'TRY',
+    'WHEN'
+]
 
 t_ATOM = r'\'[A-Za-z0-9_@\s]*\''
 t_CHAR = r'\$[A-Za-z0-9_@]'
@@ -80,7 +98,7 @@ t_RBRACE = r'\}'
 t_LABRACK = r'<'
 t_RABRACK = r'>'
 t_VBAR = r'\|'
-t_NSIGN = r'#'
+t_NSIGN = r'\#'
 t_COMMA = r','
 t_COLON = r':'
 t_SLASH = r'/'
@@ -92,6 +110,24 @@ t_MINUS = r'-'
 t_MULTI = r'\*'
 t_EXCLAIM = r'\!'
 t_APO = r'\''
+t_AFTER = r'after'
+t_APPLY = r'apply'
+t_ATTRIBUTES = r'attributes'
+t_CALL = r'call'
+t_CASE = r'case'
+t_DO = r'do'
+t_END = r'end'
+t_FUN = r'fun'
+t_IN = r'in'
+t_LET = r'let'
+t_LETREC = r'letrec'
+t_MODULE = r'module'
+t_OF = r'of'
+t_PRIMOP = r'primop'
+t_RECEIVE = r'receive'
+t_TRY = r'try'
+t_WHEN = r'when'
+
 
 t_ignore = ' \t'  # Ignore tokens
 
@@ -107,18 +143,17 @@ def t_COMMENT(t):  # No return value. Token discarded
 
 
 def t_error(t):  # Error handler
-    print("Illegal character '%s'" % t.value[0])
+    print("Illegal character '%s'. LINE %d " % (t.value[0],t.lexer.lineno))
     t.lexer.skip(1)
 
 
 # init lexer
 lexer = lex.lex()
 
-data = ''''''
-
-lexer.input(data)
-for token in lexer:
-    print(token)
+# data = ''''''
+# lexer.input(data)
+# for token in lexer:
+#     print(token)
 
 #
 #
@@ -127,79 +162,10 @@ for token in lexer:
 #
 
 
-# Atomic literal: integer, atom, nil, char and string
-def p_atomliteral_int(p):
-    'atomliteral : NUMBER'
-    p[0] = AtomicLiteral(AtomicLiteralKind.INT, p[1])
-
-
-def p_atomliteral_atom(p):
-    'atomliteral : ATOM'
-    p[0] = AtomicLiteral(AtomicLiteralKind.ATOM, p[1])
-
-
-def p_atomliteral_nil(p):
-    'atomliteral : nil'
-    p[0] = AtomicLiteral(AtomicLiteralKind.NIL, p[1])
-
-
-def p_atomliteral_char(p):
-    'atomliteral : CHAR'
-    p[0] = AtomicLiteral(AtomicLiteralKind.CHAR, p[1])
-
-
-def p_atomliteral_string(p):
-    'atomliteral : STRING'
-    p[0] = AtomicLiteral(AtomicLiteralKind.STRING, p[1])
-
-
-def p_nil(p):
-    # Nil : []
-    'nil : LBRACK RBRACK'
-    p[0] = []
-
-
-# constant: atomic literal, { } , {...} , [...] and [...|.]
-def p_constant_atom(p):
-    'constant : atomliteral'
-    p[0] = Constant(ConstantKind.ATOM, p[1])
-
-
-def p_constant_bracenone(p):
-    'constant : LBRACE RBRACE'
-    p[0] = Constant(ConstantKind.BRACE, None)
-
-
-def p_constant_brace(p):
-    'constant : LBRACE constants RBRACE'
-    p[0] = Constant(ConstantKind.BRACE, p[2])
-
-
-def p_constant_brack(p):
-    'constant : LBRACK constants RBRACK'
-    p[0] = Constant(ConstantKind.BRACK, p[2])
-
-
-def p_constant_brackvbar(p):
-    'constant : LBRACK constants VBAR constant RBRACK'
-    p[0] = Constant(ConstantKind.BRACK, p[2].append(p[4]))
-
-
-def p_constants_one(p):
-    'constants : constant'
-    p[0] = []
-    p[0].append(p[1])
-
-
-def p_constants_combine(p):
-    'constants : constants COMMA constant'
-    p[0] = p[1]
-    p[0].append(p[3])
-
-
 def p_module(p):
     'module : MODULE ATOM moduleheader modulebody END'
     # wait
+    print("done")
     pass
 
 
@@ -210,26 +176,113 @@ def p_moduleheader(p):
 
 
 def p_exports(p):
-    'exports : LBARCK functionnames RBRACK'
+    'exports : LBRACK functionnames RBRACK'
     # wait
     pass
+
+# Atomic literal: integer, atom, nil, char and string
+
+
+def p_atomliteral_int(p):
+    'atomliteral : NUMBER'
+    # p[0] = AtomicLiteral(AtomicLiteralKind.INT, p[1])
+    pass  # wait
+
+
+def p_atomliteral_atom(p):
+    'atomliteral : ATOM'
+    # p[0] = AtomicLiteral(AtomicLiteralKind.ATOM, p[1])
+    pass  # wait
+
+
+def p_atomliteral_nil(p):
+    'atomliteral : nil'
+    # p[0] = AtomicLiteral(AtomicLiteralKind.NIL, p[1])
+    pass  # wait
+
+
+def p_atomliteral_char(p):
+    'atomliteral : CHAR'
+    # p[0] = AtomicLiteral(AtomicLiteralKind.CHAR, p[1])
+    pass  # wait
+
+
+def p_atomliteral_string(p):
+    'atomliteral : STRING'
+    # p[0] = AtomicLiteral(AtomicLiteralKind.STRING, p[1])
+    pass  # wait
+
+
+def p_nil(p):
+    # Nil : []
+    'nil : LBRACK RBRACK'
+    # p[0] = []
+    pass  # wait
+
+
+# constant: atomic literal, { } , {...} , [...] and [...|.]
+def p_constant_atom(p):
+    'constant : atomliteral'
+    # p[0] = Constant(ConstantKind.ATOM, p[1])
+    pass  # wait
+
+
+def p_constant_bracenone(p):
+    'constant : LBRACE RBRACE'
+    # p[0] = Constant(ConstantKind.BRACE, None)
+    pass  # wait
+
+
+def p_constant_brace(p):
+    'constant : LBRACE constants RBRACE'
+    # p[0] = Constant(ConstantKind.BRACE, p[2])
+    pass  # wait
+
+
+def p_constant_brack(p):
+    'constant : LBRACK constants RBRACK'
+    # p[0] = Constant(ConstantKind.BRACK, p[2])
+    pass  # wait
+
+
+def p_constant_brackvbar(p):
+    'constant : LBRACK constants VBAR constant RBRACK'
+    # p[0] = Constant(ConstantKind.BRACK, p[2].append(p[4]))
+    pass  # wait
+
+
+def p_constants_one(p):
+    'constants : constant'
+    # p[0] = []
+    # p[0].append(p[1])
+    pass  # wait
+
+
+def p_constants_combine(p):
+    'constants : constants COMMA constant'
+    # p[0] = p[1]
+    # p[0].append(p[3])
+    pass  # wait
 
 
 def p_functionname(p):
     'functionname : ATOM SLASH NUMBER'
-    p[0] = FunstionName(p[1], p[3])
+    # p[0] = FunctionName(p[1], p[3])
+    pass  # wait
 
 
 def p_functionnames_one(p):
     'functionnames : functionname'
-    p[0] = []
-    p[0].append(p[1])
+    # p[0] = []
+    # p[0].append(p[1])
+    pass  # wait
 
 
 def p_functionnames_combine(p):
     'functionnames : functionnames COMMA functionname'
-    p[0] = p[1]
-    p[0].append(p[3])
+    # p[0] = p[1]
+    # p[0].append(p[3])
+    pass  # wait
 
 
 def p_attributes(p):
@@ -239,7 +292,7 @@ def p_attributes(p):
 
 
 def p_moduleattribute(p):
-    'moduleattribute : ATOM EQUAL CONSTANT'
+    'moduleattribute : ATOM EQUAL constant'
     # wait
     pass
 
@@ -275,29 +328,33 @@ def p_functiondefines_combine(p):
 
 
 def p_functiondefine(p):
-    'functiondefine : afunctionname = afun'
+    'functiondefine : afunctionname EQUAL afun'
     # wait
     pass
 
 
 def p_afunctionname_notannotated(p):
     'afunctionname : functionname'
-    p[0] = p[1]
+    # p[0] = p[1]
+    pass  # wait
 
 
 def p_afunctionname_annotated(p):
-    'afunctionname : LPAREN functionname ANNOTATION LBRACK constants RBACK RPAREN'
-    p[0] = p[2]
+    'afunctionname : LPAREN functionname ANNOTATION LBRACK constants RBRACK RPAREN'
+    # p[0] = p[2]
+    pass  # wait
 
 
 def p_afun_notannotated(p):
     'afun : fun'
-    p[0] = p[1]
+    # p[0] = p[1]
+    pass  # wait
 
 
 def p_afun_annotated(p):
     'afun : LPAREN fun ANNOTATION LBRACK constants RBRACK RPAREN'
-    p[0] = p[2]
+    # p[0] = p[2]
+    pass  # wait
 
 
 def p_expression_vlist(p):
@@ -307,6 +364,16 @@ def p_expression_vlist(p):
 
 def p_expression_singleexpression(p):
     'expression : singleexpression'
+    pass  # wait
+
+
+def p_expressions_once(P):
+    'expressions : expression'
+    pass  # wait
+
+
+def p_expressions_combine(p):
+    'expressions : expression COMMA expression'
     pass  # wait
 
 
@@ -375,15 +442,178 @@ def p_singleexpression_receive(p):
     pass  # wait
 
 
-def p_var_varname(p):
-    'var : VARNAME'
+def p_vars_avarname(p):
+    'vars : avarname'
     pass  # wait
 
 
-def p_var_varlist(p):
-    'var : LRBRACK varnames RABRACK'
+def p_vars_varlist(p):
+    'vars : LABRACK avarnames RABRACK'
     pass  # wait
 
-def p_var_empty(p):
-    'var : LRABRACK RABRACK'
-    pass # wait
+
+def p_vars_empty(p):
+    'vars : LABRACK RABRACK'
+    pass  # wait
+
+
+def p_tuple_empty(p):  # maybe conflict to empty constent
+    'tuple : LBRACE RBRACE'
+    pass  # wait
+
+
+def p_let(p):
+    'let : LET vars EQUAL expression IN expression'
+    pass  # wait
+
+
+def p_list_nobar(p):
+    'list : LBRACK expressions RBRACK'
+    pass  # wait
+
+
+def p_list_withbar(p):
+    'list : LBRACK expressions VBAR RBRACK'
+    pass  # wait
+
+
+def p_application(p):
+    'application : APPLY expression LPAREN expressions RPAREN'
+    pass  # wait
+
+
+def p_fun(p):
+    'fun : FUN LPAREN avarnames RPAREN ARROW expression'
+    pass  # wait
+
+
+def p_fun_empty(p):
+    'fun : FUN LPAREN RPAREN ARROW expression'
+    pass  # wait
+
+
+def p_apattern_var(p):
+    'apattern : avarname'
+    pass  # wait
+
+
+def p_apattern_pattern(p):
+    'apattern : pattern'
+    pass  # wait
+
+
+def p_pattern_atomicliteral(p):
+    'pattern : atomliteral'
+    pass  # wait
+
+
+def p_pattern_brace(p):
+    'pattern : LBRACE apatterns RBRACE'
+    pass  # wait
+
+
+def p_pattern_braceempty(p):
+    'pattern : LBRACE RBRACE'
+    pass  # wait
+
+
+def p_pattern_brack(p):
+    'pattern : LBRACK apatterns RBRACK'
+    pass  # wait
+
+
+def p_pattern_brackwithbar(p):
+    'pattern : LBRACK apatterns VBAR apattern RBRACK'
+    pass  # wait
+
+
+def p_pattern_varname(p):
+    'pattern : avarname EQUAL apattern'
+    pass  # wait
+
+
+def p_avarname_varname(p):
+    'avarname : VARNAME'
+    pass  # wait
+
+
+def p_avarnames_once(p):
+    'avarnames : avarname'
+    pass  # wait
+
+
+def p_avarnames_combine(p):
+    'avarnames : avarnames COMMA avarname'
+    pass  # wait
+
+
+def p_apatterns_once(p):
+    'apatterns : apattern'
+    pass  # wait
+
+
+def p_apatterns_combine(p):
+    'apatterns : apatterns COMMA apattern'
+    pass  # wait
+
+
+def p_patterns_pattern(p):
+    'patterns : apattern'
+    pass  # wait
+
+
+def p_patterns_patternlist(p):
+    'patterns : LABRACK apatterns RABRACK'
+    pass  # wait
+
+
+def p_patterns_empty(p):
+    'patterns : LABRACK RABRACK'
+    pass  # wait
+
+
+def p_clause(p):
+    'clause : patterns guard ARROW expression'
+    pass  # wait
+
+
+def p_guard(p):
+    'guard : WHEN expression'
+    pass  # wait
+
+
+def p_aclause(p):
+    'aclause : clause'
+    pass  # wait
+
+
+def p_case(p):
+    'case : CASE expression OF aclauses END'
+    pass  # wait
+
+
+def p_aclauses_once(p):
+    'aclauses : aclause'
+    pass  # wait
+
+
+def p_aclauses_combine(p):
+    'aclauses : aclauses aclause'
+    pass  # wait
+
+
+def p_receive(p):
+    'receive : RECEIVE aclauses timeout'
+    pass  # wait
+
+
+def p_timeout(p):
+    'timeout : AFTER expression ARROW expression'
+    pass  # wait
+
+
+parser = yacc.yacc()
+
+filein = open("examples/pipe.core")
+data = filein.read()
+result = parser.parse(data, lexer=lexer)
